c4de9c88ef0f02d849565df469e8076a35a6df93
tregubowww <tregubowww@gmail.com>
2020-11-18 07:46:36 +0300

    movie details

--- app/src/main/java/ru/myuniquenickname/myapplication/MainActivity.kt
+++ app/src/main/java/ru/myuniquenickname/myapplication/MainActivity.kt
@@ -1,11 +1,18 @@
 package ru.myuniquenickname.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val intent = Intent(this, MovieDetailsActivity ::class.java)
+        val  textView = findViewById<TextView>(R.id.next)
+         textView.setOnClickListener{startActivity(intent)}
     }
 }
