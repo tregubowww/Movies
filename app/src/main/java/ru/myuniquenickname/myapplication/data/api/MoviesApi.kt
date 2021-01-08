9e469c1dca50781fb23e53bbabb81e3f908a6fe2
pcfaktor <pcfaktor@gmail.com>
2021-01-08 23:09:56 +0300

    fix: move the api_key query parameter to the interceptor

--- app/src/main/java/ru/myuniquenickname/myapplication/data/api/MoviesApi.kt
+++ app/src/main/java/ru/myuniquenickname/myapplication/data/api/MoviesApi.kt
@@ -3,39 +3,35 @@ package ru.myuniquenickname.myapplication.data.api
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
-import ru.myuniquenickname.myapplication.data.dataMapping.ActorListDto
-import ru.myuniquenickname.myapplication.data.dataMapping.GenresListDto
-import ru.myuniquenickname.myapplication.data.dataMapping.ImageDto
-import ru.myuniquenickname.myapplication.data.dataMapping.MovieDetailsDto
-import ru.myuniquenickname.myapplication.data.dataMapping.MoviesDto
+import ru.myuniquenickname.myapplication.data.dataMapping.*
 
 interface MoviesApi {
-    @GET("movie/popular?api_key=3b4970990228d308657e91ae5af780b1")
+    @GET("movie/popular")
     suspend fun getMoviesPopular(): MoviesDto
 
-    @GET("movie/top_rated?api_key=3b4970990228d308657e91ae5af780b1")
+    @GET("movie/top_rated")
     suspend fun getMoviesTopMovies(): MoviesDto
 
-    @GET("movie/upcoming?api_key=3b4970990228d308657e91ae5af780b1")
+    @GET("movie/upcoming")
     suspend fun getMoviesUpcoming(): MoviesDto
 
-    @GET("search/movie/?api_key=3b4970990228d308657e91ae5af780b1")
+    @GET("search/movie")
     suspend fun getMoviesSearch(
         @Query("query") movie: String
     ): MoviesDto
 
-    @GET("genre/movie/list?api_key=3b4970990228d308657e91ae5af780b1")
+    @GET("genre/movie/list")
     suspend fun getGenres(): GenresListDto
 
-    @GET("configuration?api_key=3b4970990228d308657e91ae5af780b1")
+    @GET("configuration")
     suspend fun getImage(): ImageDto
 
-    @GET("movie/{id}?api_key=3b4970990228d308657e91ae5af780b1")
+    @GET("movie/{id}")
     suspend fun getMovie(
         @Path("id") id: Long
     ): MovieDetailsDto
 
-    @GET("movie/{id}/credits?api_key=3b4970990228d308657e91ae5af780b1")
+    @GET("movie/{id}/credits")
     suspend fun getActors(
         @Path("id") id: Long,
     ): ActorListDto
